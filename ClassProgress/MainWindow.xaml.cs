4d33968f9a59991db3de2c37b497c2c856d4e57b
DivyeGala <divyegala@gmail.com>
2016-07-24 22:11:31 +0530

    Add project files.

---
+++ ClassProgress/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassProgress
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string text,student="student",teacher="teacher";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string id = textBox.Text;
+            string password = passwordBox.Password;
+            string url = "http://192.168.0.107/sooad/getdetails.php?id=" + id + "&password=" + password;
+            //label.Content = url;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Method = WebRequestMethods.Http.Get;
+            httpWebRequest.Accept = "application/json";
+            var response = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+            }
+            label.Content = text;
+
+            if(text.ToString().Equals(student))
+            {
+                Controls.Content = StudentPage;
+            }
+            else if (text.ToString().Equals(teacher))
+            {
+
+            }
+            else
+            {
+                label.Content = "Username/Password is incorrect.";
+            }
+        }
+    }
+}
